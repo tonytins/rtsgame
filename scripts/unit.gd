3527fa64a20979d8758d4d80d1aa90b6350d19e0
Rico <drraccoony@gmail.com>
2020-06-07 17:07:06 -0500

    Selection Manager via WorldManager

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -1,19 +1,27 @@
 extends KinematicBody2D
 
-export var selected = false
+export var selected = false setget set_selected
 onready var box = $box
 
+signal was_selected
+signal was_deselected
+
+func set_selected(value):
+	if selected != value:
+		selected = value
+		box.visible = value
+		if selected:
+			emit_signal("was_selected", self)
+		else:
+			emit_signal("was_deselected", self)
+
 func _ready():
-	pass # Replace with function body.
+	connect("was_selected", get_parent(), "select_unit")
+	connect("was_deselected", get_parent(), "deselect_unit")
+	pass
 
 
 func _process(delta):
-	if selected:
-		if box.visible == false:
-			box.visible = true
-	else:
-		if box.visible == true:
-			box.visible = false
 	pass
 
 
@@ -21,7 +29,6 @@ func _on_unit_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			if event.button_index == BUTTON_LEFT:
-				selected = true
+				set_selected(true)
 			if event.button_index == BUTTON_RIGHT:
-				selected = false
-	pass
+				set_selected(false)
