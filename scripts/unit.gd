2c646da62c97ea7659529e1dbba2f17ba0b9a363
Rico <drraccoony@gmail.com>
2020-06-07 17:52:17 -0500

    Started UI button work

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 export var selected = false setget set_selected
 onready var box = $box
+onready var bar = $bar
+onready var unitname = $unitname
 
 signal was_selected
 signal was_deselected
@@ -10,12 +12,17 @@ func set_selected(value):
 	if selected != value:
 		selected = value
 		box.visible = value
+		unitname.visible = value
+		bar.visible = value
 		if selected:
 			emit_signal("was_selected", self)
 		else:
 			emit_signal("was_deselected", self)
 
 func _ready():
+	randomize()
+	unitname.text = name
+	bar.value = randi() % 90 + 10
 	connect("was_selected", get_parent(), "select_unit")
 	connect("was_deselected", get_parent(), "deselect_unit")
 	pass
--- scripts/worldManager.gd
+++ scripts/worldManager.gd
@@ -2,11 +2,32 @@ extends Node2D
 
 var selected_units = []
 
+onready var button = preload("res://scenes/Button.tscn")
+var buttons = []
+
 func select_unit(unit):
 	if not selected_units.has(unit):
 		selected_units.append(unit)
 	print("selected %s" % unit.name)
+	create_buttons()
 func deselect_unit(unit):
 	if selected_units.has(unit):
 		selected_units.erase(unit)
 	print("deselected %s" % unit.name)
+	create_buttons()
+
+func create_buttons():
+	delete_buttons()
+	for unit in selected_units:
+		if not buttons.has(unit.name):
+			var but = button.instance()
+			but.connect_me(self, unit.name)
+			but.rect_position()
+
+func delete_buttons():
+	for but in buttons:
+		if "UI/Base".has_node(but):
+			var b = $"UI/Base".get_node(but)
+			b.queue_free()
+			$"UI/Base".remove_child(b)
+	buttons.clear()
