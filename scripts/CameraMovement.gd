aed776faa002ea7f0d01466281767a90964b1ba1
Rico <drraccoony@gmail.com>
2020-06-07 16:11:23 -0500

    Camera Zoom

--- scripts/CameraMovement.gd
+++ scripts/CameraMovement.gd
@@ -1,6 +1,14 @@
 extends Camera2D
 
-export var speed = 10.0
+export var speed = 25.0
+export var zoomspeed = 50.0
+export var zoommargin = 0.3
+
+export var zoomMin = 0.5
+export var zoomMax = 3.0
+
+var zoompos = Vector2()
+var zoomfactor = 1.0
 
 
 func _ready():
@@ -11,7 +19,28 @@ func _process(delta):
 	# Smooth Movement
 	var inputx = (int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")))
 	var inputy = (int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up")))
-	position.x += inputx * speed
-	position.y += inputy * speed
-	print(inputx)
+	position.x = lerp(position.x, position.x + inputx * speed * zoom.x, speed * delta)
+	position.y = lerp(position.y, position.y + inputy * speed * zoom.y, speed * delta)
+	
+	# Zooming
+	zoom.x = lerp(zoom.x, zoom.x * zoomfactor, zoomspeed * delta)
+	zoom.y = lerp(zoom.y, zoom.y * zoomfactor, zoomspeed * delta)
+	
+	zoom.x = clamp(zoom.x, zoomMin, zoomMax)
+	zoom.y = clamp(zoom.y, zoomMin, zoomMax)
 	pass
+
+func _input(event):
+	if abs(zoompos.x - get_global_mouse_position().x) > zoommargin:
+		zoomfactor = 1.0
+	if abs(zoompos.y - get_global_mouse_position().y) > zoommargin:
+		zoomfactor = 1.0
+	
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				zoomfactor -= 0.01
+				zoompos = get_global_mouse_position()
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				zoomfactor += 0.01
+				zoompos = get_global_mouse_position()
